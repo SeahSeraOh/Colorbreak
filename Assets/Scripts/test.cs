48252dbcbf79758759eb3a19f81fa513435eb604
SeahSeraOh <seraoh199@naver.com>
2022-09-07 15:07:01 +0200

    2

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //private int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+
+       // GUI.Label(new Rect(10, 10, 200, 100), health.ToSring();
+
+       // if (GUI.Button(new Rect(10, 50, 500, 500), "Click net")) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // health -= -1;
+    }
+}
